ad23c833da8cc4cd71c4eab654a04f17b37283c1
Swagath <mr.swagath72@gmail.com>
2026-03-28 12:57:51 +0530

    connected backend and fixed secret leak in build

--- src/api.ts
+++ src/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:8000";
+export const BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://180.235.121.253:8125";
 
 const API = axios.create({
   baseURL: BASE_URL,
